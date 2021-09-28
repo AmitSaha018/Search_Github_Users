79d8a5ad0c23949b6010b7993a2651d5d317f563
AmitSaha018 <31251262+AmitSaha018@users.noreply.github.com>
2021-09-28 10:47:17 +0530

    before showing stars and forks funcationality

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { GithubContext } from '../context/context';
+import { GlobalGitHubContext } from '../context/context';
 import styled from 'styled-components';
 
 const Followers = () => {
-  return <h2>followers component</h2>;
+  const { followers } = GlobalGitHubContext()
+  return (
+    <Wrapper>
+      <div className='followers'>
+        {followers.map((followers, index) => {
+          const { avatar_url: img, html_url, login } = followers;
+          return (
+            <article key={index}>
+              <img src={img} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>{ html_url }</a>
+              </div>
+            </article>
+          )
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.article`
